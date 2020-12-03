4385eb4067a1fa0a5307d1c42b3d02298b2ba9c0
AnaLefinir <analefinir@gmail.com>
2020-12-03 11:25:52 -0300

    add dto and deleteTask

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v1 as uuid } from 'uuid';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Injectable()
 export class TasksService {
@@ -10,7 +11,13 @@ export class TasksService {
         return this.tasks;
     }
 
-    createTask(title: string, description: string): Task {
+    getTaskById(id: string): Task {
+        return this.tasks.find(t => t.id === id);
+    }
+
+    createTask(createTaskDto : CreateTaskDto): Task {
+        const { title, description } = createTaskDto;
+
         const task: Task = {
             id: uuid(),
             title,
@@ -20,5 +27,12 @@ export class TasksService {
 
         this.tasks.push(task);
         return task;
+    }
+
+    deleteTask(id: string): Task {
+        const taskToDelete = this.tasks.find(t => t.id === id);
+        this.tasks = this.tasks.filter(t => t.id !== taskToDelete.id);
+
+        return taskToDelete;
     }
 }
