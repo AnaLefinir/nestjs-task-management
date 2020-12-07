88c1b92b61105f05fd8f51932385c51a8fd1ed0b
AnaLefinir <analefinir@gmail.com>
2020-12-07 17:24:16 -0300

    fix connection string

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { TasksController } from './tasks/tasks.controller';
 import { TasksService } from './tasks/tasks.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './config/typeorm.config';
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    TypeOrmModule.forRoot(typeOrmConfig),
+    TasksModule],
   controllers: [TasksController],
   providers: [TasksService],
 })
